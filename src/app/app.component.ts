db699deb17ec9e8d8345ea20e2a6331f2d98eee1
Anuj Singh <anuj_singh@epam.com>
2019-11-22 18:46:39 +0530

    Todo list created with todo input box working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Directive, Input, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todos';
+
+  @ViewChild("todoInput",{static: false}) todoInput : ElementRef;
+
+  todos = [
+    {title : "Read Angular", description : "from angular docs"},
+    {title : "Drink water", description : "after every 1 hour"},
+    {title : "Eat Fruits", description : "Atleast once in a day"}
+    ];
+
+    inputTodoHandler(event : Event){
+      let todo = this.todoInput.nativeElement.value;
+      if(todo.length > 0)
+      this.todos.push({title : todo, description : "Default one"});
+    }
 }
